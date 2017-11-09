c88daa069fcc95188e4c0be6d11022a126a9e3b7
JONATHAN FRANCO <Franco@JONATHANs-MBP.home>
2017-11-09 12:00:01 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+const config = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /(\.css)$/, 
+        loaders: ['style-loader', 'css-loader']
+      }
+
+    ]
+  }
+}
+
+module.exports = config
